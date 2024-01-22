ff8a5ea467ddede532fe24c627e49f4d9562daea
Edgar Roman Cervantes <redvant@outlook.com>
2024-01-21 21:52:15 -0600

    Add LinkParse with getHref

--- parser.go
+++ parser.go
@@ -5,13 +5,41 @@ import (
 	"io"
 )
 
-func LinkParse(r io.Reader) string {
+type Link struct {
+	Href string
+	Text string
+}
+
+func HtmlLinkParse(r io.Reader) []Link {
 	doc, err := html.Parse(r)
 	if err != nil {
 		panic(err)
 	}
+	return linkParse(doc)
+}
 
-	return getText(doc)
+func linkParse(n *html.Node) []Link {
+	var links []Link
+	if n.Type == html.ElementNode && n.Data == "a" {
+		l := Link{}
+		l.Href = getHref(n)
+		l.Text = getText(n)
+		links = append(links, l)
+		return links
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = append(links, linkParse(c)...)
+	}
+	return links
+}
+
+func getHref(n *html.Node) string {
+	for _, at := range n.Attr {
+		if at.Key == "href" {
+			return at.Val
+		}
+	}
+	return ""
 }
 
 func getText(n *html.Node) string {
--- parser_test.go
+++ parser_test.go
@@ -2,9 +2,50 @@ package linkparser
 
 import (
 	"golang.org/x/net/html"
+	"reflect"
 	"strings"
 	"testing"
 )
+
+func TestLinkParse(t *testing.T) {
+	cases := []struct {
+		Description   string
+		Html          string
+		ExpectedParse []Link
+	}{
+		{
+			Description:   "parse link",
+			Html:          "<a href=\"test.url\">test</a>",
+			ExpectedParse: []Link{{Href: "test.url", Text: "test"}},
+		},
+		{
+			Description:   "parse multiple links",
+			Html:          "<a href=\"test.link\">link1</a><a>link2</a>",
+			ExpectedParse: []Link{
+				{Href: "test.link", Text: "link1"},
+				{Text: "link2"}},
+		},
+		{
+			Description: "return nil if no anchor tags",
+			Html: "<p>test</p><h1>moreinfo</h1><span>1</span>",
+			ExpectedParse: nil,
+		},
+		{
+			Description: "only parse anchor tags",
+			Html: "<p>test</p><a href=\"test.url\">test<span>1</span></a><span>1</span>",
+			ExpectedParse: []Link{{Href: "test.url", Text: "test1"}},
+		},
+	}
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			r := strings.NewReader(test.Html)
+			got := HtmlLinkParse(r)
+			if !reflect.DeepEqual(got, test.ExpectedParse) {
+				t.Errorf("got %v, want %v", got, test.ExpectedParse)
+			}
+		})
+	}
+}
 
 func TestGetText(t *testing.T) {
 	cases := []struct {
